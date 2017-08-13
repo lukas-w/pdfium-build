ad6879c9e2ed3388c07e53743503f44d8bf8e1b3
Lukas W <lukaswhl@gmail.com>
2017-08-13 12:39:53 +0200

    Add update json script

---
+++ update-json.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# Uses pip packages: b2, PyGithub
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from datetime import datetime
+import json
+import os
+import sys
+
+from b2.api import B2Api
+realm_url = 'production'
+
+from github import Github
+repo = Github(os.environ['GITHUB_TOKEN']).get_repo('Lukas-W/pdfium')
+
+api = B2Api()
+api.authorize_account(realm_url, os.environ['B2_ACCOUNT_ID'], os.environ['B2_APP_KEY'])
+bucket = api.get_bucket_by_name(os.environ['B2_BUCKET'])
+files = bucket.list_file_names()['files'];
+
+file_dictionary = {};
+
+for file in files:
+	file_name = file['fileName'];
+	if file_name == 'index.json':
+		continue
+
+	sha, system, base_name = file_name.split('/');
+	size = file['size'];
+	timestamp = datetime.fromtimestamp(file['uploadTimestamp'] / 1000);
+
+
+	if sha not in file_dictionary:
+		file_dictionary[sha] = {'files': []}
+
+	file_dictionary[sha]['files'].append({
+		'name': base_name,
+		'system': system,
+		'size': size,
+		'timestamp': timestamp.isoformat(),
+		'download_url': bucket.get_download_url(file_name)
+	})
+
+for sha in file_dictionary:
+	commit = repo.get_commit(sha).commit
+	file_dictionary[sha]['author_date'] = commit.author.date.isoformat();
+	file_dictionary[sha]['committer_date'] = commit.committer.date.isoformat();
+
+json_string = json.dumps(file_dictionary, indent=4)
+
+u = json_string.encode('utf-8')
+bucket.upload_bytes(u, 'index.json', 'application/json')
+
+print("Uploaded index.json", file=sys.stderr)
+print(json_string)
